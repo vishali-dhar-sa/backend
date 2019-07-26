b10228d6a7f961855747c9b8260899ab66a1eba5
vishali dhar <vishali.dhar.sa@gmail.com>
2019-07-26 20:25:19 +0530

    third commit

--- app.js
+++ app.js
@@ -6,19 +6,32 @@ cors=require('cors');
 mongoose = require("mongoose");
 User = require('./model/user');
 Post =require('./model/post');
-upload=require('./service/upload');
+multer=require('multer');
+multerS3=require('multer-s3');
+aws=require('aws-sdk');
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200,
+     // some legacy browsers (IE11, various SmartTVs) choke on 204 
+    }
+    
+app.use(cors(corsOptions));
+// app.use(function(req, res, next) {
+//     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     next();
+// });
+var k;
 
+mongoose.connect("mongodb://localhost:27017/publicshare_app" ,{ useNewUrlParser: true });
 
-mongoose.connect("mongodb://localhost/publicshare_app");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var corsOptions = {
-    origin: 'http://localhost:4200',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-    }
-    
-    app.use(cors(corsOptions))
+
+
 
 
 app.post('/api/signup',(req, res) => {
@@ -70,37 +83,60 @@ app.post('/api/login',(req, res) => {
         // if(!value){
         // console.log("Sorry! password is wrong")
     // }
-})  
+})
 
-// var singleUpload=upload.single('image');
-// app.post('/image-upload',function(req,res){
-//     singleUpload(req,res,function(err){
-//         return res.json({'imageUrl':req.file.location})
-//     })
-// })
+aws.config.update({
+    secretAccessKey: 'ZkGH0h2hCM27hH/zJ7ghGjSSqVjjtOOWjbt3v5he',
+    accessKeyId: 'AKIAILQS6MVEVPBOXDQA',
+    region: 'us-east-2',
+    ACL:'public-read'
+});
 
-// app.post('/api/postcreate',function(res,res){
-//     var title=req.body.title;
-//     var description=req.body.description;
 
-//     var postData={
-//         title:title,
-//         description:description
-//     }
-//     Post.create(postData,function(err,post){
-//         if(err){
-//             console.log(err)
-//         }else{
-//             console.log(post)
-//         }
+s3 = new aws.S3();
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'upload-project-images',
+        key: function (req, file, cb) {
+            cb(null,file.originalname);
+        }
+        
+    })
     
-//     })    
+});
 
-// })
-   
+app.post('/api/upload',upload.array('file'), function (req, res, next) {
+    var title=req.body.title;
+    var description=req.body.description;
+    
+    var fileURL= 'https://s3.console.aws.amazon.com/s3/buckets/upload-project-images/'+req.body.file;
+console.log(upload.array('file'));
+    console.log(title,description,fileURL)
 
+    var postData={
+        title:title,
+        description:description,
+        file:fileURL
+    }
 
+    Post.create(postData,function(err,post){
+        if(err){
+            console.log(err)
+        }if(!post){
+            console.log('no posts available')
+        }else{
+            console.log(post)
+        }
+    })
+    res.send("Uploaded!");
+});
 
+app.get('/postcreate',(req,res)=>{
+  res.send({
+      message: "Hello"
+  })
+});
 
 app.listen(8880, function () {
     console.log("server has started");
--- model/user.js
+++ model/user.js
@@ -1,7 +1,11 @@
 var mongoose=require("mongoose")
 
 var UserSchema=new mongoose.Schema({
-    email:String,
+    email:{
+        type:String,
+        trim: true,
+        lowercase: true,
+    },
     password:String,
 })
 
